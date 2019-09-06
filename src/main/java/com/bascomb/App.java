343a9b0d5de4808a4e56b513588d075ec18704a6
bascomb.arnette <bascomb.arnette@calheers.ca.gov>
2019-09-06 12:28:02 -0700

    Spring Configuration : Main Method : webapp type none

--- src/main/java/com/bascomb/App.java
+++ src/main/java/com/bascomb/App.java
@@ -1,15 +1,26 @@
 package com.bascomb;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //ApplicationContext context = SpringApplication.run(App.class, args);
+        SpringApplicationBuilder sb = new SpringApplicationBuilder(App.class);
+        sb.web(WebApplicationType.NONE);
+        ConfigurableApplicationContext context = sb.run(args);
+        System.out.println();
     }
 }
